03ee6ab4f0ae5209e0c46a75ad6129d36cbd91d2
Samet <sametirkoren@gmail.com>
2022-02-19 16:58:02 +0300

    Product listing and components were created.

---
+++ client/src/app/features/catalog/Catalog.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import { Product } from "../../models/product";
+import ProductList from "./ProductList";
+
+export default function Catalog() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("https://localhost:5000/api/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <>
+      <ProductList products={products} />
+    </>
+  );
+}
